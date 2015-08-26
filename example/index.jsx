0794c995a34e763b3879d7ded2bd0e3389fa1d17
Tyler Bushnell <tyler@interlincx.com>
2015-08-25 19:02:51 -0700

    now implemented as a react component

--- example/index.jsx
+++ example/index.jsx
@@ -1,69 +1,8 @@
 
 var React = require('react')
-var ReactCR = require('..')
-//var ReactCR = require('../lib/index.jsx')
+var ReactCR = require('../index.js')
 
 
-  React.render(
-    (
-      <div>
-        <h1>React Country/Region Select</h1>
-
-        <p>
-          Making country / region selection suck less.
-        </p>
-
-        <div id="countryHolder">
-        </div>
-        <div id="provinceHolder">
-        </div>
-      </div>
-    ),
-    document.body,
-
-    function(){
-
-      opts = {
-        countryDefault: 'US',
-        country:{
-          name:'countrySelect',
-          label:'Country',
-          value:'US',
-          twoLetterValues:true,
-          el:document.getElementById("countryHolder")
-          },
-        province:{
-          name:'provinceSelect',
-          label:'State/Province',
-          value:'CA',
-          twoLetterValues:true,
-          el:document.getElementById("provinceHolder")
-          },
-        defaultCss: true
-      }
-
-      rcp = new ReactCR(opts)
-/*
-      opts = {
-        country:true,
-        countryName:'countrySelect',
-        countryLabel:'Country',
-        countryValue:'US',
-        countryEl:document.getElementById("countryHolder"),
-        province:true,
-        provinceName:'provinceSelect',
-        provinceLabel:'State/Province',
-        provinceValue:'CA',
-        provinceEl:document.getElementById("provinceHolder"),
-        defaultCss: true
-      }
-
-      React.render(React.createElement(ReactCR, opts))
-*/
-    }
-  )
-
-/*
   React.render(
     (
       <div>
@@ -81,4 +20,4 @@ var ReactCR = require('..')
 
     document.body
   )
-*/
+
